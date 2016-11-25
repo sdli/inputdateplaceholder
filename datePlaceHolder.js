46d14ca5e8374307390dcd86d1a604584624a5cd
steven <steven@ydongj.com>
2016-11-25 18:09:03 +0800

    'steven20161125'

--- datePlaceHolder.js
+++ datePlaceHolder.js
@@ -1,15 +1,3 @@
-### inputdateplaceholder
-你可以复制以下代码，或下载dateplaceholder.js进行使用！
-可以解决手机端（ios和android）web的datepicker控件加载时不显示日期的问题。
-
-```javascript
-//css内容
-input[type="date"]:before{
-	content: attr(placeholder);
-	color:#4d4d4d;
-}
-
-//js内容
 $(function(){
     var datePicker = $('input[type=date]');
     for(var i = 0;i<datePicker.length;i++){
@@ -23,4 +11,3 @@ $(function(){
         })(i);
     }
 });
-```
